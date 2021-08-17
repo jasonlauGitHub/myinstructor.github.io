5dbb44f4955b54db46cccd979249d876e9122c73
Andre Specht <andrespecht@outlook.com>
2021-08-17 09:55:56 -0500

    OOP Week #3

--- Class files/OOP/Week 3/Arrays_02.java
+++ Class files/OOP/Week 3/Arrays_02.java
@@ -6,6 +6,7 @@ public class Arrays_02 {
     // 0 - 5
     int[] a1 = {1, 2, 3, 4, 5, 6};
 
+    // i -> iterator
     for (int i = 0; i < a1.length; i++) {
       System.out.print(a1[i] + " ");
     }
